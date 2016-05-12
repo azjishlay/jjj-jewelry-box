9413bcbd80c5578ae3592a8907f6b33cf1d97f26
lijasminej <li.jasmine.j@gmail.com>
2016-05-12 12:39:44 -0400

    Update server, add index

--- server.js
+++ server.js
@@ -11,18 +11,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-// Set up handlebars for express
+// set up handlebars for express
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+    extname: '.handlebars',
+    layoutsDir: 'app/views/layouts'
+}));
 app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/app/views');
 
 // load the static files
-var staticContentFolder = __dirname + '/public';
+var staticContentFolder = __dirname + '/app/public';
 app.use(express.static(staticContentFolder));
 
 // require the api and html paths
-require("./routes/apiRoutes.js")(app)
-require("./routes/htmlRoutes.js")(app)
+require("./app/routes/apiRoutes.js")(app)
+require("./app/routes/htmlRoutes.js")(app)
 
 // start the server
 var PORT = process.env.PORT || 8080;
