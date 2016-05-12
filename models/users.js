e69c0c107b156becd1786228fe2a3a3389bbc9c4
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-05-11 22:59:42 -0400

    adding jeromes users.js

---
+++ models/users.js
@@ -0,0 +1,44 @@
+// require the sequelize npm app
+var Sequelize = require("sequelize"); 
+
+// require the connection to the db
+var sequelize = require("../config/connection.js"); 
+
+// "group" model that matches up with DB
+var employees = sequelize.define("employees", {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	user_name: {
+		type: Sequelize.STRING
+	},
+	password: {
+		type: Sequelize.STRING
+	},
+	photo: {
+		type: Sequelize.STRING
+	},
+	name: {
+		type: Sequelize.STRING
+	},
+	gender: {
+		type: Sequelize.STRING
+	},
+	sales: {
+		type: Sequelize.INTEGER
+	},
+	sales_goals: {
+		type: Sequelize.INTEGER
+	}
+});
+
+// Syncs with DB
+employees.sync({force: true}).then(function () {
+  // Table created
+  console.log('employees table created')
+});
+
+// Makes the employees Model available for other files (will also create a table)
+module.exports = employees;
