158d5068f97ca472903e842df0144d7615ec2e23
lijasminej <li.jasmine.j@gmail.com>
2016-05-20 20:06:20 -0400

    Add loggedIn function to htmlRoutes.js

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -3,8 +3,19 @@ var invoices = require('../models/invoices.js');
 var line_items = require('../models/line_items.js');
 var products = require('../models/products.js');
 var product_categories = require('../models/product_categories.js');
+var clients = require('../models/clients');
 var passport = require('passport');
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
 
+// Generic function to redirect if user is not logged in
+function loggedIn(req, res, next) {
+	if (req.user) {
+		next();
+	} else {
+		res.redirect('/login');
+	}
+}
 
 module.exports = function(app){
 	
@@ -34,41 +45,41 @@ module.exports = function(app){
 		res.redirect('/');
 	});
 
-	app.get('/clients', function(req, res){
-	res.render('create-client');
-	});
-
-	app.get('/products', function(req, res){
-	res.render('products');
-	});
-
-	app.get('/create-product', function(req, res){
-	res.render('products-add-new');
-	});
-
-	app.get('/invoices', function(req, res){
-	res.render('invoices');
-	});
-
-	app.get('/create-invoice', function(req, res){
-		res.render('create-invoice');
-	});
-
-	app.get('/work-orders', function(req, res){
-	res.render('work-orders');
-	});
-
-	app.get('/create-work-order', function(req, res){
-		res.render('create-work-order');
-	});
-
-	app.get('/employees', function(req, res){
-	res.render('employees');
-	});
-
-	app.get('/create-employee', function(req, res){
-		res.render('create-employee');
-	});
+	// app.get('/clients', function(req, res){
+	// res.render('clients');
+	// });
+    //
+	// app.get('/products', function(req, res){
+	// res.render('products');
+	// });
+    //
+	// app.get('/create-product', function(req, res){
+	// res.render('products-add-new');
+	// });
+    //
+	// app.get('/invoices', function(req, res){
+	// res.render('invoices');
+	// });
+    //
+	// app.get('/create-invoice', function(req, res){
+	// 	res.render('create-invoice');
+	// });
+    //
+	// app.get('/work-orders', function(req, res){
+	// res.render('work-orders');
+	// });
+    //
+	// app.get('/create-work-order', function(req, res){
+	// 	res.render('create-work-order');
+	// });
+    //
+	// app.get('/employees', function(req, res){
+	// res.render('employees');
+	// });
+    //
+	// app.get('/create-employee', function(req, res){
+	// 	res.render('create-employee');
+	// });
 
 	app.post('/login', passport.authenticate('local'), function(req, res){
 		res.redirect('/');
