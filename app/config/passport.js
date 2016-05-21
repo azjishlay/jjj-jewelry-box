af1fe2fda21a577faaeb7fece459b5c9fad7d200
jcarless <carless.jerome@gmail.com>
2016-05-20 23:43:07 -0400

    updated graph and added comments

--- app/config/passport.js
+++ app/config/passport.js
@@ -8,28 +8,35 @@ var employees = require('../models/users.js');
 var invoices = require('../models/invoices.js');
 var products = require('../models/products.js');
 
+var lowEnd = 0;
+var mid = 0;
+var highEnd = 0;
+var luxury = 0;
+var sub = 0;
 
 
 
 module.exports = function(app){
 	// cookieParser/expressSession middleware needed to feed passport correctly
-app.use(cookieParser());
+app.use(cookieParser()); //cookie parser holds the session id
+	// session secret prevents anyone from guessing session ids and accessing other users
+	// server will know if secret has been changed and deny access
 app.use(expressSession({ secret: process.env.SESSION_SECRET || 'secret',
 	resave: false,
 	saveUninitialized: false 
 }));
 
 	// passport middleware needs to be placed after all parsing to work correctly
+	// initialize is default passport middleware
 app.use(passport.initialize());
-app.use(passport.session());
+	// session puts data into local authentication
+	app.use(passport.session());
 passport.use(new passportLocal.Strategy(function(username, password, done){
 	// teach passport how to authenticate users
-	// add code to check username and password with DB here
 
 	employees.findAll({ where: {user_name: username, password: password }}).then(function(result){
 
-
-		// if(!Error) {
+				// checking db for username and password match
 			if (username === result[0].user_name && password === result[0].password) {
 
 				done(null, {
@@ -45,10 +52,7 @@ passport.use(new passportLocal.Strategy(function(username, password, done){
 			} else{
 				done(null, null);
 			}
-		// } else {
-		// 		// done(new Error('login failed'));
-		// 		console.log('error: ' + Error);
-		// 	}
+
 				});
 
 	}));
@@ -62,33 +66,34 @@ passport.deserializeUser(function(id, done){
 	employees.findAll({ where: {id: id}}).then(function(result){
 		invoices.findAll({ where: {employee_id: result[0].id}}).then(function(result2){
 
-				var lowEnd = 0;
-				var mid = 0;
-				var highEnd = 0;
-				var luxury = 0;
-				var sub = 0;
+
 invoices.findAll({ attributes: [[sequelize.fn('COUNT', sequelize.col('subtotal')), 'subtotals']], where: {employee_id: result[0].id} }).then(function(result3){
 	console.log(result3[0].dataValues.subtotals);
 
-			// console.log("result:" + JSON.parse(result2[0]));
+	 lowEnd = 0;
+	 mid = 0;
+	 highEnd = 0;
+	 luxury = 0;
+	 sub = 0;
 
+			// count invoice tiers
 				for (i = 0; i < result3[0].dataValues.subtotals; i++){
 					sub = parseInt(result2[i].subtotal);
 					if (sub < 500){
-						lowEnd += sub;
+						lowEnd += 1;
 					}else if (sub < 1000 && sub >= 500){
-						mid += sub;
+						mid += 1;
 					}else if(sub < 5000 && sub >= 1000){
-						highEnd += sub;
+						highEnd += 1;
 					}else if (sub >= 5000){
-						luxury += sub;
+						luxury += 1;
 					}else{
 						console.log("error");
 					}
 				}
 
-				var performance = Math.round((result[0].sales / result[0].sales_goals) * 100);
-
+	var performance = Math.round((result[0].sales / result[0].sales_goals) * 100);
+				// make all variables
 			done(null, {id: result[0].id, name: result[0].name, sales: result[0].sales, sales_goal: result[0].sales_goals, photo: result[0].photo, performance: performance, lowEnd: lowEnd, mid: mid, highEnd: highEnd, luxury: luxury });
 });
 		})
